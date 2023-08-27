38292d9069cb9266168691dec562cd068c860290
seohyeon <psh970709@gmail.com>
2023-08-27 23:11:29 +0900

    고유한 id 기능 추가

--- src/App.js
+++ src/App.js
@@ -11,38 +11,48 @@ function App() {
     { id: 1, title: "투두리스트는", body: "영어로 To Do List", isDone: true }
   ])
 
+  //제목, 내용 받아오기 위한 코드
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
 
+  //고유한 id를 만들기 위해 추가
+  const [nextId, setNextId] = useState(2);
+
+  //제목 저장
   const titleChangeHandler = (event) => {
     setTitle(event.target.value)
   }
 
+  //내용 저장
   const bodyChangeHandler = (event) => {
     setBody(event.target.value)
   }
 
+  //+ 버튼을 눌러 글 추가(고유한 아이디 생성 부분)
   const clickAddButtonHandler = () => {
     const newList = {
-      id: list.length + 1,
+      id: nextId,
       title,
       body,
       isDone: false
     }
-    //2. newList 투두리스트 추가 (제목, 내용 비어있으면 alert 띄우기)   
+    //newList 투두리스트 추가 (제목, 내용 비어있으면 alert 띄우기)   
     if (title.length !== 0 && body.length !== 0) {
       setList([...list, newList])
       setTitle("")
       setBody("")
+      setNextId(nextId + 1);
     } else {
       alert('빈 칸을 채워주세요')
     }
   }
 
+  //삭제
   const clickDeleteButtonHandler = (id) => {
     setList(list.filter(list => list.id !== id))
   }
 
+  //Done으로 변경
   const clickDoneButtonHandler = (id) => {
     setList(list.map(list => {
       if (list.id === id) {
@@ -51,6 +61,8 @@ function App() {
       return list
     }))
   }
+
+  //working으로 변경
   const clickWorkingButtonHandler = (id) => {
     setList(list.map(list => {
       if (list.id === id) {
@@ -120,6 +132,7 @@ function App() {
             )
           })}
       </div>
+      <button onClick={aa}>확인</button>
     </div>
   );
 }
